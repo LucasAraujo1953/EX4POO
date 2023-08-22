022ea60ebd3196ef5c444b6c182a96ee27db0cad
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-22 18:41:45 -0300

    Add files via upload

---
+++ EX4POO/EX4POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a base do triângulo: ");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do triângulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(baseTriangulo, altura);
+
+            double area = triangulo.CalcularArea();
+            Console.WriteLine($"A área do triângulo é: {area}");
+        }
+    }
+}
---
+++ EX4POO/EX4POO/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4POO
+{
+    internal class Triangulo
+    {
+        private double baseTriangulo;
+        private double altura;
+
+        public Triangulo(double baseTriangulo, double altura)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.altura = altura;
+        }
+
+        public double CalcularArea()
+        {
+            return (baseTriangulo * altura) / 2;
+        }
+    }
+}
+
